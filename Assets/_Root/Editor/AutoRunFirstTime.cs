08e7964c3c1ce8f7f4d3af5d71a892966cb34a35
Quyetnd96 <quyetnd@gameestudio.com>
2022-10-21 21:50:24 +0700

    cancle namespace

--- Assets/_Root/Editor/AutoRunFirstTime.cs
+++ Assets/_Root/Editor/AutoRunFirstTime.cs
@@ -2,20 +2,20 @@ using System;
 using UnityEditor;
 using UnityEngine;
 
-namespace Pancake.DailyReward
+// namespace Pancake.DailyReward
+// {
+[InitializeOnLoad]
+internal class AutoRunFirstTime
 {
-    [InitializeOnLoad]
-    internal class AutoRunFirstTime
+    static AutoRunFirstTime()
     {
-        static AutoRunFirstTime()
+        Debug.Log("1");
+        if (!EditorPrefs.GetBool($"__Example__{PlayerSettings.productGUID}", false))
         {
-            Debug.Log("1");
-            if (!EditorPrefs.GetBool($"__Example__{PlayerSettings.productGUID}", false))
-            {
-                Debug.Log("2");
-                ImportPackage.ImportExample();
-                EditorPrefs.SetBool($"__Example__{PlayerSettings.productGUID}", true);
-            }
+            Debug.Log("2");
+            ImportPackage.ImportExample();
+            EditorPrefs.SetBool($"__Example__{PlayerSettings.productGUID}", true);
         }
     }
 }
+// }
