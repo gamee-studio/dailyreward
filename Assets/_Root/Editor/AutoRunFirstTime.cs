04140af0f2952ddbd295ba85cc7a300f1446a0fb
Quyetnd96 <quyetnd@gameestudio.com>
2022-10-24 10:03:17 +0700

    done task

--- Assets/_Root/Editor/AutoRunFirstTime.cs
+++ Assets/_Root/Editor/AutoRunFirstTime.cs
@@ -12,23 +12,8 @@ namespace Pancake.DailyReward
             if (!EditorPrefs.GetBool($"__Example__{PlayerSettings.productGUID}", false))
             {
                 ImportPackage.ImportExample();
-
-                Check();
-            }
-        }
-        private static void Check()
-        {
-            var editorResourcePath = "Assets/ExampleScene/";
-            if (!editorResourcePath.DirectoryExists())
-            {
-                Debug.Log("you dont have that folder");
-            }
-            else
-            {
-                Debug.Log("done import");
                 EditorPrefs.SetBool($"__Example__{PlayerSettings.productGUID}", true);
             }
-
         }
     }
 
